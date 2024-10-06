ce343135f75fda96511d3b5257234e173199f246
evgluzedev <evgeniy.shostak.04@mail.ru>
2024-10-06 21:54:32 +0300

    refactoring

--- src/main/java/com/example/bpmsenterprise/components/authentication/entity/User.java
+++ src/main/java/com/example/bpmsenterprise/components/authentication/entity/User.java
@@ -1,34 +1,29 @@
 package com.example.bpmsenterprise.components.authentication.entity;
 
-import com.example.bpmsenterprise.components.authentication.token.Token;
+import com.example.bpmsenterprise.components.authentication.token.*;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
+import lombok.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.authority.*;
+import org.springframework.security.core.userdetails.*;
 
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 @Entity
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="user")
+@Table(name = "user")
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name="id")
+    @Column(name = "id")
     private Integer id;
 
-    @Column(name="firstname")
+    @Column(name = "firstname")
     private String firstname;
 
-    @Column(name="lastname")
+    @Column(name = "lastname")
     private String lastname;
 
     @Column(name = "birth_day")
@@ -48,6 +43,10 @@ public class User implements UserDetails {
 
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    public static UserBuilder builder() {
+        return new UserBuilder();
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
@@ -86,4 +85,78 @@ public class User implements UserDetails {
 
     @OneToMany(mappedBy = "user")
     private List<Token> tokens;
+
+    public static class UserBuilder {
+        private Integer id;
+        private String firstname;
+        private String lastname;
+        private String birth_day;
+        private String password;
+        private String email;
+        private String phone;
+        private String surname;
+        private Role role;
+        private List<Token> tokens;
+
+        UserBuilder() {
+        }
+
+        public UserBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public UserBuilder firstname(String firstname) {
+            this.firstname = firstname;
+            return this;
+        }
+
+        public UserBuilder lastname(String lastname) {
+            this.lastname = lastname;
+            return this;
+        }
+
+        public UserBuilder birth_day(String birth_day) {
+            this.birth_day = birth_day;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder phone(String phone) {
+            this.phone = phone;
+            return this;
+        }
+
+        public UserBuilder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public UserBuilder role(Role role) {
+            this.role = role;
+            return this;
+        }
+
+        public UserBuilder tokens(List<Token> tokens) {
+            this.tokens = tokens;
+            return this;
+        }
+
+        public User build() {
+            return new User(this.id, this.firstname, this.lastname, this.birth_day, this.password, this.email, this.phone, this.surname, this.role, this.tokens);
+        }
+
+        public String toString() {
+            return "User.UserBuilder(id=" + this.id + ", firstname=" + this.firstname + ", lastname=" + this.lastname + ", birth_day=" + this.birth_day + ", password=" + this.password + ", email=" + this.email + ", phone=" + this.phone + ", surname=" + this.surname + ", role=" + this.role + ", tokens=" + this.tokens + ")";
+        }
+    }
 }
--- src/main/java/com/example/bpmsenterprise/components/authentication/services/AuthenticationService.java
+++ src/main/java/com/example/bpmsenterprise/components/authentication/services/AuthenticationService.java
@@ -34,6 +34,8 @@ public class AuthenticationService implements UserData {
                 .email(request.getEmail())
                 .password(passwordEncoder.encode(request.getPassword()))
                 .role(Role.USER).build();
+
+
         var savedUser = repository.save(user);
         var jwtToken = jwtService.generateToken(user);
         saveUserToken(savedUser, jwtToken);
